c3119020157a6a941ba1228031b75206c4b64c55
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2023-02-27 16:21:00 +0100

    complete project

--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.superheroes
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.data.JetpackHeroes
+import com.example.superheroes.model.Superhero
 import com.example.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +36,166 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SuperheroesApp()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SuperheroesApp() {
+    Scaffold(
+        topBar = {
+            SuperheroTopBar()
+        }
+    ) {
+        HeroesList(list = JetpackHeroes.heroes)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    SuperheroesTheme {
-        Greeting("Android")
+fun SuperheroTopBar(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .height(56.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h1
+        )
+    }
+}
+
+@Composable
+fun HeroesList(
+    modifier: Modifier = Modifier,
+    list: List<Superhero>
+) {
+    LazyColumn(
+        modifier = modifier
+            //.padding(16.dp)
+            .background(
+                color = MaterialTheme.colors.background
+            ),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(list) {
+            SuperheroItem(hero = it)
+        }
+    }
+}
+
+@Composable
+fun SuperheroItem(
+    modifier: Modifier = Modifier,
+    hero : Superhero
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            /*.padding(
+                start = 16.dp,
+                end = 16.dp,
+                top = 8.dp,
+                bottom = 8.dp
+            )*/
+            //.height(72.dp)
+            .clip(shape = MaterialTheme.shapes.medium),
+        elevation = 2.dp
+    ) {
+        Column() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+            ) {
+                SuperheroInfo(
+                    name = hero.name,
+                    powers = hero.powers,
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(Modifier.width(16.dp))
+                SuperheroImage(
+                    image = hero.image,
+                    description = hero.name
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SuperheroInfo(
+    modifier: Modifier = Modifier,
+    @StringRes name : Int,
+    @StringRes powers : Int
+) {
+    Column(
+        modifier = modifier
+            //.fillMaxWidth(1f)
+            .padding(
+                end = 16.dp
+            )
+    ) {
+        Text(
+            text = stringResource(id = name),
+            style = MaterialTheme.typography.h3,
+            textAlign = TextAlign.Left
+        )
+        Text(
+            text = stringResource(id = powers),
+            style = MaterialTheme.typography.body1,
+            textAlign = TextAlign.Left,
+            overflow = TextOverflow.Clip
+        )
+    }
+}
+
+@Composable
+fun SuperheroImage(
+    modifier: Modifier = Modifier,
+    @DrawableRes image : Int,
+    @StringRes description : Int
+) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = stringResource(id = description),
+        modifier = Modifier
+            .height(72.dp)
+            .width(72.dp)
+            .clip(
+                shape = MaterialTheme.shapes.small
+            ),
+        contentScale = ContentScale.FillBounds
+    )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun LightThemePreview() {
+    SuperheroesTheme(darkTheme = false) {
+        SuperheroesApp()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun DarkThemePreview() {
+    SuperheroesTheme(darkTheme = true) {
+        SuperheroesApp()
     }
 }
--- app/src/main/java/com/example/superheroes/ui/theme/Theme.kt
+++ app/src/main/java/com/example/superheroes/ui/theme/Theme.kt
@@ -7,24 +7,21 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = md_theme_dark_primary,
+    secondary = md_theme_dark_secondary,
+    background = md_theme_dark_background,
+    surface = md_theme_dark_surface,
+    onPrimary = md_theme_dark_onPrimary,
+    onSurface = md_theme_dark_onSurface
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = md_theme_light_primary,
+    secondary = md_theme_light_secondary,
+    background = md_theme_light_background,
+    surface = md_theme_light_surface,
+    onPrimary = md_theme_light_onPrimary,
+    onSurface = md_theme_light_onSurface
 )
 
 @Composable
