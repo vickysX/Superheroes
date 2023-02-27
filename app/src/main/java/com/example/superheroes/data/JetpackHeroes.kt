9f5009495af9be59bf465112af68b9bff4fb6020
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2023-02-27 14:03:14 +0100

    add resources and data

---
+++ app/src/main/java/com/example/superheroes/data/JetpackHeroes.kt
@@ -0,0 +1,39 @@
+package com.example.superheroes.data
+
+import com.example.superheroes.R
+import com.example.superheroes.model.Superhero
+
+object JetpackHeroes {
+    val heroes = listOf(
+        Superhero(
+            R.string.nick,
+            R.string.nick_powers,
+            R.drawable.android_superhero1
+        ),
+        Superhero(
+            R.string.reality_protector,
+            R.string.reality_protector_powers,
+            R.drawable.android_superhero2
+        ),
+        Superhero(
+            R.string.andre,
+            R.string.andre_powers,
+            R.drawable.android_superhero3
+        ),
+        Superhero(
+            R.string.benjamin,
+            R.string.benjamin_powers,
+            R.drawable.android_superhero4
+        ),
+        Superhero(
+            R.string.maru,
+            R.string.maru_powers,
+            R.drawable.android_superhero5
+        ),
+        Superhero(
+            R.string.yasmine,
+            R.string.yasmine_powers,
+            R.drawable.android_superhero6
+        )
+    )
+}
